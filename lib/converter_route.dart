088befbe1d49546b5c26dbef121da0964f58412c
Ankit Shah <ankit.shah@medwing.com>
2020-05-08 12:21:55 +0200

    Convert widget to stateful

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -3,23 +3,29 @@ import 'package:meta/meta.dart';
 
 import 'package:tutorial_1/unit.dart';
 
-class ConverterRoute extends StatelessWidget {
-  final List<Unit> units;
+class ConverterRoute extends StatefulWidget {
   final Color color;
 
+  final List<Unit> units;
+
   const ConverterRoute({
-    @required this.units,
     @required this.color,
-  }) : assert(units != null),
-  assert(color != null);
+    @required this.units,
+  })  : assert(color != null),
+        assert(units != null);
 
   @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  @override
   Widget build(BuildContext context) {
-    final unitWidgets = units.map((Unit unit) {
+    final unitWidgets = widget.units.map((Unit unit) {
       return Container(
+        color: widget.color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
-        color: color,
         child: Column(
           children: <Widget>[
             Text(
